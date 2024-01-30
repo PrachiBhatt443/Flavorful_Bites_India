2952718f68f58c4b4cd159ee56ca82bfa23466cb
Prachi Bhatt <bprachi443@gmail.com>
2024-01-30 17:43:07 +0530

    Updated files

--- food/src/App.js
+++ food/src/App.js
@@ -1,11 +1,24 @@
 import Navbar from "./components/Navbar/Navbar";
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Blogs from "./components/Blogs/Blogs";
+import Homepage from "./components/Navbar/HomePage";
+import Recepie from "./components/Recepie/Recepie";
 function App() {
-  return (
-    <div className="App">
-      <Navbar/>  
-    </div>
+  return ( 
+    <Router>
+      <div className="App">
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Homepage/>}/>
+          <Route path="/blogs" element={<Blogs/>} />
+          {/* <Route path="/recepie" element={<Recepie index={1}/>} /> */}
+          {/* <Route path="/recepie/:index" component={Recepie} /> */}
+          <Route path="recepie">
+            <Route path=":id" element={<Recepie/>}/>
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
-
 export default App;
--- food/src/components/Navbar/Navbar.jsx
+++ food/src/components/Navbar/Navbar.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import styles from './Navbar.module.css'
-import Button from '../Button/Button'
+import React, { useState, useEffect } from 'react';
+import styles from './Navbar.module.css';
+import { Link } from 'react-router-dom';
+
 function Navbar() {
-  return (    
-    <nav>
-        <div >
-            <img className={styles.logo} src='./img/cooking1.png'/>
+  return (
+      <nav>
+        <div>
+          <img className={styles.logo} src='./img/cooking1.png' alt="Logo" />
         </div>
-      <ul>
-        <li href="#">Homepage</li>
-        <li href="#">Receipe Page</li>
-        <li href="#">Blogs</li>
-        <li href="#">Buy</li>
-      </ul>
-      <button className={styles.red}>
-        Sign In
-      </button>
-    </nav>
+        <ul>
+        <li><Link to='/'>Homepage</Link></li>
+          <li><Link to='/recepie/{0}'>Recipe Page</Link></li>
+          {/* <li><Link to={'/recepie'}>Recipe Page</Link></li> */}
+          <li><Link to="/blogs">Blogs</Link></li>
+          <li><Link to="#">Buy</Link></li>
+        </ul>
+        <button className={styles.red}>Sign In</button>
+      </nav>
+    
   )
 }
+
 export default Navbar
