c3884597407e3e74f8d96349905021cf8aa75606
Prachi Bhatt <bprachi443@gmail.com>
2024-02-26 09:13:05 +0530

    Recepie page updated

--- food/src/components/Recepie/Recepie.jsx
+++ food/src/components/Recepie/Recepie.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import VdoBlock from './VdoBlock'
 import { useParams } from 'react-router-dom'
-
+import B1 from './B1'
+import styles from './B1.module.css'
 
 const Recepie = (props) =>{
   const {id} = useParams();
@@ -9,6 +10,16 @@ const Recepie = (props) =>{
   return (
     <>
       { id!=-1 && <VdoBlock  Index={parseInt(id)}/>}
+      <div className={styles.nav}>
+        <B1 name='Poori' icons={require('../Navbar/images/Card/img1.jpg')} index={0}/>
+        <B1 name='Pav Bhaji'  icons={require('../Navbar/images/Card/img2.jpg')} index={1}/>
+        <B1 name='Cheese Cake' icons={require('../Navbar/images/Card/img3.jpg')} index={2}/>
+        <B1 name='Burger' icons={require('../Navbar/images/Card/img4.jpg')} index={3}/>
+        <B1 name='Cappuccino' icons={require('../Navbar/images/Card/img1.jpg')} index={0}/>
+        <B1 name='Pav Bhaji'  icons={require('../Navbar/images/Card/img2.jpg')} index={1}/>
+        <B1 name='Cheese Cake' icons={require('../Navbar/images/Card/img3.jpg')} index={2}/>
+        <B1 name='Burger' icons={require('../Navbar/images/Card/img4.jpg')} index={3}/>
+      </div>
       <footer></footer>
     </>
   )
